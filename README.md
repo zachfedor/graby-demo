739da62bb2b2e00221f2a6d0c5da225de8c49adc
zachfedor <zachfedor@gmail.com>
2018-02-02 12:45:50 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Graby Demo
+
+Quick test of [Graby's](https://github.com/j0k3r/graby) article extraction and cleanup.
+
+## Build
+
+Ensure you have PHP and Composer installed.
+
+```
+git clone git@github.com:zachfedor/graby-demo.git
+cd graby-demo
+composer install
+```
+
+## Run
+
+```
+php -S localhost:8000
+```
+
+And visit [localhost:8000]().
---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+  require __DIR__ . '/vendor/autoload.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>graby demo</title>
+
+  <style>
+    form {
+      width: 50%;
+      margin: 20px auto;
+      min-width: 500px;
+    }
+    input, textarea {
+      display: block;
+      width: 100%;
+    }
+    textarea {
+      min-height: 200px;
+      margin-bottom: 20px;
+    }
+    h2 {
+      border-bottom: 1px solid black;
+      padding: 20px;
+      margin: 40px 0;
+    }
+    pre {
+      background: #eee;
+      padding: 20px;
+      overflow: scroll;
+    }
+  </style>
+</head>
+<body>
+  <form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
+    profile url: <input type="text" name="url"><br>
+    html: <textarea name="html"></textarea>
+    <input type="submit">
+  </form>
+
+  <?php
+  if (!empty($_POST)):
+    $graby = new Graby\Graby();
+    $result = $graby->cleanupHtml($_POST["html"], $_POST["url"]);
+  ?>
+    <h2>Output</h2>
+
+    <?php echo $result; ?>
+
+    <h2>Raw HTML</h2>
+
+    <pre><code>
+      <?php echo htmlspecialchars($result); ?>
+    </pre></code>
+
+  <?php endif; ?>
+</body>
+</html>
